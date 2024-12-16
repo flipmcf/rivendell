a0768af00bb577b89de28eaff0793b3d3cb3dece
Mike McFadden <mcfaddenm@rfa.org>
2024-12-16 16:17:21 -0500

    template for rdcae tests - but needs to be manually compiled and run

---
+++ lib/test/check_rdcae.cpp
@@ -0,0 +1,143 @@
+
+#include "gtest/gtest.h"
+
+
+
+TEST(RdcaeTest, connectHost){
+    FAIL();
+}
+
+TEST(RdcaeTest, enableMetering){
+    FAIL();
+}
+
+TEST(RdcaeTest, loadPlay){
+    FAIL();
+}
+
+TEST(RdcaeTest, unloadPlay){
+    FAIL();
+}
+
+TEST(RdcaeTest, positionPlay){
+    FAIL();
+}
+
+TEST(RdcaeTest, play){
+    FAIL();
+}
+
+TEST(RdcaeTest, stopPlay){
+    FAIL();
+}
+
+
+TEST(RdcaeTest, loadRecord){
+    FAIL();
+}
+
+
+TEST(RdcaeTest, unloadRecord){
+    FAIL();
+}
+
+TEST(RdcaeTest, record){
+    FAIL();
+}
+
+TEST(RdcaeTest, stopRecord){
+    FAIL();
+}
+
+TEST(RdcaeTest, setClockSource){
+    FAIL();
+}
+
+TEST(RdcaeTest, setInputVolume){
+    FAIL();
+}
+
+TEST(RdcaeTest, setOutputVolume){
+    FAIL();
+}
+
+TEST(RdcaeTest, fadeOutputVolume){
+    FAIL();
+}
+
+TEST(RdcaeTest, setInputLevel){
+    FAIL();
+}
+
+TEST(RdcaeTest, setOutputLevel){
+    FAIL();
+}
+
+TEST(RdcaeTest, setInputMode){
+    FAIL();
+}
+
+TEST(RdcaeTest, setOutputMode){
+    FAIL();
+}
+
+TEST(RdcaeTest, setInputVOXLevel){
+    FAIL();
+}
+
+TEST(RdcaeTest, setInputType){
+    FAIL();
+}
+
+TEST(RdcaeTest, setPassthroughVolume){
+    FAIL();
+}
+
+TEST(RdcaeTest, inputStatus){
+    FAIL();
+}
+
+TEST(RdcaeTest, inputMeterUpdate){
+    FAIL();
+}
+
+TEST(RdcaeTest, outputMeterUpdate){
+    FAIL();
+}
+
+TEST(RdcaeTest, outputStreamMeterUpdate){
+    FAIL();
+}
+
+TEST(RdcaeTest, playPosition){
+    FAIL();
+}
+
+TEST(RdcaeTest, requestTimescale){
+    FAIL();
+}
+
+TEST(RdcaeTest, playPortStatus){
+    FAIL();
+}
+
+TEST(RdcaeTest, readyData){
+    FAIL();
+}
+
+TEST(RdcaeTest, SendCommand){
+    FAIL();
+}
+
+TEST(RdcaeTest, DispatchCommand){
+    FAIL();
+}
+
+
+TEST(RdcaeTest, UpdateMeters){
+    FAIL();
+}
+
+TEST(RdcaeTest, SerialCheck){
+    FAIL();
+}
